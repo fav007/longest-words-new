04c31171d0c93e2d44a819a01449dc22d9b3354e
Prodigy <besthorizon@hotmail.fr>
2022-01-21 10:51:45 +0300

    modif calc ok

--- 01-OOP/02-System-Parameters/calc.py
+++ 01-OOP/02-System-Parameters/calc.py
@@ -5,8 +5,9 @@ def main():
     # We need to import argv inside the main() body to make our tests pass
     # Importing in the main function will force Python to reload argv between each tests
     from sys import argv
+    
+    return eval(argv[1] + argv[2] + argv[3])
 
-    pass  # TODO: implement the calculator
 
 
 if __name__ == "__main__":
