6cfa24eb589c00209500c7600321d8dd55885f5b
Prodigy <besthorizon@hotmail.fr>
2022-01-21 13:13:05 +0300

    ok"

--- 01-OOP/06-Data-Structures/currencies.py
+++ 01-OOP/06-Data-Structures/currencies.py
@@ -2,8 +2,17 @@
 # pylint: disable=fixme
 # pylint: disable=unused-argument
 
-RATES = {} # TODO: add some currency rates
+from logging import captureWarnings
+
+
+RATES = {"USDEUR":0.85,"GBPEUR":1.13,"CHFEUR":0.86} # TODO: add some currency rates
 
 # `amount` is a `tuple` like (100, EUR). `currency` is a `string`
 def convert(amount, currency):
+    val,currency_dep=amount
+    clef=currency_dep+currency
+    if currency_dep not in "".join(RATES):
+        return None
+    rate=RATES[clef]
+    return round(val*rate,0)
     pass # TODO
