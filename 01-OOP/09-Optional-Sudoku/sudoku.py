c5b5cb878dd414e96e7570d7efc9760200b8fc98
Prodigy <besthorizon@hotmail.fr>
2022-01-21 15:28:03 +0300

    ok'

--- 01-OOP/09-Optional-Sudoku/sudoku.py
+++ 01-OOP/09-Optional-Sudoku/sudoku.py
@@ -5,5 +5,18 @@ class SudokuSolver:
         self.grid = grid
 
     def is_valid(self):
-        # TODO: implement this!
-        pass
+        for i in range(9):
+            if len(set([self.grid[i][j] for j in range(9)])) != 9 :
+                return False
+            if len(set([self.grid[j][i] for j in range(9)])) != 9 :
+                return False
+        for u in range(0,9,3):
+            for v in range(0,9,3):
+                if len(set([self.grid[i][j] for i in range(u,u+3) for j in range(v,v+3) ]))!=9:
+                    return False
+        return True
+        
+                
+                
+        
+        
