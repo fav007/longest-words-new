0da7eb2b8d43d34e64027f7ad62f2e0a4291d652
Prodigy <besthorizon@hotmail.fr>
2022-01-21 14:21:18 +0300

    ok+10

--- 01-OOP/01-Sum-Of-Three/sum_of_three.py
+++ 01-OOP/01-Sum-Of-Three/sum_of_three.py
@@ -1,5 +1,4 @@
 # pylint: disable=missing-docstring
 
 def sum3(first_number, second_number, third_number):
-    
-    return first_number+second_number+third_number
+    return first_number + second_number + third_number
