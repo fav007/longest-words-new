59572e25a5015fc4dd7a7d9cdce29e7ecc2f108a
Prodigy <besthorizon@hotmail.fr>
2022-01-21 13:33:46 +0300

    okok

--- 01-OOP/07-Error-Handling/square.py
+++ 01-OOP/07-Error-Handling/square.py
@@ -1,6 +1,10 @@
 # pylint: disable=missing-docstring
 # pylint: disable=fixme
 import sys
-
 if __name__ == "__main__":
-    print(type(sys.argv[1]))
+    #print(type(sys.argv[1]))*
+    try: 
+        print((int(sys.argv[1]))**2)
+        
+    except:
+        print("An error occurred.")
